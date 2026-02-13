24e58133817b6d201dd52795af48f559c97765f5
alabro-bm <atanas.labroski@brightmarbles.io>
2026-02-13 18:51:52 +0100

    Showing the weapon now

--- ShooterBRemake/Assets/Scripts/Controllers/ShooterController.cs
+++ ShooterBRemake/Assets/Scripts/Controllers/ShooterController.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace ShooterB
 {
     public class ShooterController : MonoBehaviour
     {
+        [System.Serializable]
+        public class WeaponIconEntry
+        {
+            public Constants.WeaponType weaponType;
+            public Sprite icon;
+        }
+
         [Header("Weapons")]
         public Weapon activeWeapon;
 
@@ -11,28 +19,78 @@ namespace ShooterB
         public GameObject rifleBulletPrefab;
         public Sprite defaultRifleIcon;
 
+        [Header("Weapon Icons")]
+        public List<WeaponIconEntry> weaponIcons = new List<WeaponIconEntry>();
+
+        private void Awake()
+        {
+            EnsureActiveWeapon();
+        }
+
         private void Start()
         {
-            if (activeWeapon == null)
-            {
-                GameObject rifleObj = new GameObject("Rifle");
-                rifleObj.transform.SetParent(transform);
-                Rifle rifle = rifleObj.AddComponent<Rifle>();
-
-                if (rifleBulletPrefab != null)
-                {
-                    rifle.bulletPrefab = rifleBulletPrefab;
-                }
-
-                if (defaultRifleIcon != null && rifle.weaponIcon == null)
-                {
-                    rifle.weaponIcon = defaultRifleIcon;
-                }
-
-                activeWeapon = rifle;
-            }
+            EnsureActiveWeapon();
 
             StartCoroutine(LogInitialization());
+        }
+
+        private void EnsureActiveWeapon()
+        {
+            if (activeWeapon != null)
+            {
+                ApplyIconIfMissing(activeWeapon);
+                return;
+            }
+
+            GameObject rifleObj = new GameObject("Rifle");
+            rifleObj.transform.SetParent(transform);
+            Rifle rifle = rifleObj.AddComponent<Rifle>();
+
+            if (rifleBulletPrefab != null)
+            {
+                rifle.bulletPrefab = rifleBulletPrefab;
+            }
+
+            if (defaultRifleIcon != null && rifle.weaponIcon == null)
+            {
+                rifle.weaponIcon = defaultRifleIcon;
+            }
+
+            activeWeapon = rifle;
+            ApplyIconIfMissing(activeWeapon);
+
+            if (activeWeapon == null)
+            {
+                Debug.LogError("[SHOOTER] Failed to initialize active weapon.");
+            }
+        }
+
+        private void ApplyIconIfMissing(Weapon weapon)
+        {
+            if (weapon == null || weapon.weaponIcon != null)
+                return;
+
+            Sprite mappedIcon = FindIconForWeaponType(weapon.weaponType);
+            if (mappedIcon != null)
+            {
+                weapon.weaponIcon = mappedIcon;
+            }
+            else if (weapon.weaponType == Constants.WeaponType.Rifle && defaultRifleIcon != null)
+            {
+                weapon.weaponIcon = defaultRifleIcon;
+            }
+        }
+
+        private Sprite FindIconForWeaponType(Constants.WeaponType weaponType)
+        {
+            for (int i = 0; i < weaponIcons.Count; i++)
+            {
+                WeaponIconEntry entry = weaponIcons[i];
+                if (entry != null && entry.weaponType == weaponType && entry.icon != null)
+                    return entry.icon;
+            }
+
+            return null;
         }
 
         private System.Collections.IEnumerator LogInitialization()
