06b311044e3ef933e26c7bfe2687de24168c7271
alabro-bm <atanas.labroski@brightmarbles.io>
2026-04-28 01:37:58 +0200

    feat: add DuckPartConfig struct

---
+++ ShooterBRemake/Assets/Scripts/Models/Ducks/DuckPartConfig.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace ShooterB
+{
+    [Serializable]
+    public struct DuckPartConfig
+    {
+        public Sprite torsoSprite;
+        public Sprite wingSprite;
+        public Sprite rightWingSprite;
+        public Vector2 leftWingPivotOffset;
+        public Vector2 rightWingPivotOffset;
+        public float flapSpeed;
+        public float flapAmplitude;
+        public float phaseOffset;
+        public float torsoBobAmount;
+        public float torsoBobSpeed;
+    }
+}
